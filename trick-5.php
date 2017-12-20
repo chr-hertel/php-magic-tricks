898340e3102c8d2dc3266853d81a240c170c0681
Tony Lemke <tlemke@naji-dev.de>
2017-12-20 21:42:13 +0100

    Update trick-5.php

--- trick-5.php
+++ trick-5.php
@@ -1,15 +1,25 @@
 <?php
 
-/* Insert Magic Code Here */
+$data = new class implements Countable
+{
+
+    public function count()
+    {
+        return 0;
+    }
+
+    public function __toString()
+    {
+        return 'Hello';
+    }
+
+};
 
 // NO CHANGES ALLOWED BELOW
-
 for ($i = 0; $i <= count($data); $i++) {
     $data++;
 }
-
 echo $data;
-
 /*
  * Magic output:
  *
