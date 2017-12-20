110b47d16665f2fd76cd3d3e2b768baf91bde3af
Andreas Möller <am@localheinz.com>
2017-12-20 20:14:36 +0100

    Enhancement: Add solution for trick 6

---
+++ solution-trick6.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(ticks=3);
+
+register_tick_function(function () {
+    echo ' PHP ';
+});
+
+// NO CHANGES ALLOWED BELOW
+
+echo 'Hello';
+echo 'World';
+
+/*
+ * Magic output:
+ *
+ * Hello PHP World
+ */
